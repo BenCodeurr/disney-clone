0f3f9dfa0859ca34919cede27ef07f924ef22286
benCodeur <benedictobed20@gmail.com>
2023-12-11 12:02:04 +0000

    Fully responsive header successfully done

--- src/components/HeaderItems.jsx
+++ src/components/HeaderItems.jsx
@@ -3,7 +3,7 @@ export default function HeaderItems({name, Icon}) {
   return (
     <div className="flex gap-3 items-center text-[15px] font-semibold cursor-pointer hover:underline underline-offset-8 mb-2">
       <Icon/>
-      <h2 className="hidden md:block">{name}</h2>
+      <h2 className="">{name}</h2>
     </div>
   )
 }
